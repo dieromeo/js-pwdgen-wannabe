723be9bacd8fd3bfe24fc3afde73606f560797b1
dieromeo <diegoromeo99@icloud.com>
2023-10-31 15:51:22 +0100

    feat: password generator with static number

--- myscript.js
+++ myscript.js
@@ -1,2 +1,21 @@
 'use strict';
 
+let elementName = 'nome';
+let elementLastName = null;
+let elementFavouriteColor = null;
+const elementNumber = 21;
+const elementPassword = document.getElementById('your-password');
+
+//console.log(elementNumber)
+
+elementName = prompt('Inserisci il tuo nome: ');
+elementLastName = prompt('Type your lastname');
+elementFavouriteColor = prompt('Type your favourite color');
+
+console.log(elementName);
+console.log(elementLastName);
+console.log(elementFavouriteColor);
+
+elementPassword.innerHTML = elementName + elementLastName + elementFavouriteColor + elementNumber;
+
+console.log(elementPassword.innerHTML)
